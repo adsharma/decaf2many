e67c24b1bfcd95730cbe1a6fc16771a93e5acb00
Arun Sharma <arun@sharma-home.net>
2021-08-11 12:18:24 -0700

    Basic transpiler structure

--- decaf2many/cli.py
+++ decaf2many/cli.py
@@ -3,33 +3,28 @@ import argparse
 import sys
 
 
-from antlr4 import *
+from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
+from .decaf2many import Transpiler
 from .lang.JavaLexer import JavaLexer
-from .lang.JavaListener import JavaListener
 from .lang.JavaParser import JavaParser
 
-class JavaPrintListener(JavaListener):
-    def enterCompilationUnit(self, ctx):
-        print("Got here")
-
-def run_one():
-    lexer = JavaLexer(StdinStream())
+def run_one(filename):
+    lexer = JavaLexer(FileStream(filename))
     stream = CommonTokenStream(lexer)
     parser = JavaParser(stream)
     tree = parser.compilationUnit()
-    printer = JavaPrintListener()
+    tx = Transpiler()
     walker = ParseTreeWalker()
-    walker.walk(printer, tree)
+    walker.walk(tx, tree)
 
 
-def main(args):
+def main():
     """Console script for decaf2many."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args, rest = parser.parse_known_args(args=args)
+    args, rest = parser.parse_known_args()
 
     return run_one(rest[0])
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.args))  # pragma: no cover
+    sys.exit(main())  # pragma: no cover
--- decaf2many/decaf2many.py
+++ decaf2many/decaf2many.py
@@ -1,1 +1,7 @@
 """Main module."""
+
+from .lang.JavaParserListener import JavaParserListener
+
+class Transpiler(JavaParserListener):
+    def enterCompilationUnit(self, ctx):
+        print("Got here")
