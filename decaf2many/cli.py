d7744db26e654bb2f6d5ccd254698547a3caa7ac
Arun Sharma <arun@sharma-home.net>
2021-08-11 13:28:09 -0700

    Handle HelloWorld

--- decaf2many/cli.py
+++ decaf2many/cli.py
@@ -3,28 +3,31 @@ import argparse
 import sys
 
 from antlr4 import CommonTokenStream, FileStream
+from antlr4.tree.Trees import Trees
 
 from .decaf2many import Transpiler
 from .lang.JavaLexer import JavaLexer
 from .lang.JavaParser import JavaParser
 
 
-def run_one(filename):
+def run_one(args, filename):
     lexer = JavaLexer(FileStream(filename))
     stream = CommonTokenStream(lexer)
     parser = JavaParser(stream)
     tree = parser.compilationUnit()
+    if args.dump:
+        print(Trees.toStringTree(tree, None, parser))
+        return
     tx = Transpiler()
-    tx.visit(tree)
+    out = tx.visit(tree)
+    print(out)
+    return
 
 
-def main():
+def main(args=None):
     """Console script for decaf2many."""
     parser = argparse.ArgumentParser()
-    args, rest = parser.parse_known_args()
+    parser.add_argument("--dump", default=False, action="store_true", help="dump ast instead of transpiling")
+    args, rest = parser.parse_known_args(args=args)
 
-    return run_one(rest[0])
-
-
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    return run_one(args, rest[0])
--- decaf2many/decaf2many.py
+++ decaf2many/decaf2many.py
@@ -1,8 +1,115 @@
 """Main module."""
 
+import textwrap
+from typing import DefaultDict
 from .lang.JavaParserVisitor import JavaParserVisitor
+from .lang.JavaParser import JavaParser
 
 
 class Transpiler(JavaParserVisitor):
-    def visitCompilationUnit(self, ctx):
-        print("Got here")
+    DISPATCH_MAP = {"println": "print"}
+    INDENT = " " * 4
+    TYPE_MAP = {
+        "String": "str",
+    }
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def indent(self, text):
+        return textwrap.indent(text, self.INDENT)
+
+    def defaultResult(self):
+        return ""
+
+    def aggregateResult(self, aggregate, nextResult):
+        return aggregate + nextResult
+
+    def _dispatch(self, fname):
+        if fname in self.DISPATCH_MAP:
+            return self.DISPATCH_MAP[fname]
+        return fname
+
+    def visitComment(self, comment, parser):
+        if comment.type == parser.LINE_COMMENT:
+            return comment.text.replace("//", "#")
+        return comment.text
+
+    def visitComments(self, ctx):
+        pos = ctx.getSourceInterval()[0]
+        comments = ctx.parser.getTokenStream().getHiddenTokensToLeft(pos)
+        comments_str = [self.visitComment(c, ctx.parser) for c in comments]
+        return "\n".join(comments_str)
+
+    def visitCompilationUnit(self, ctx: JavaParser.CompilationUnitContext):
+        c  = self.visitComments(ctx)
+        return c + "\n".join([self.visit(t) for t in ctx.typeDeclaration()])
+
+    def visitClassDeclaration(self, ctx: JavaParser.ClassDeclarationContext):
+        name = ctx.IDENTIFIER().getText()
+        body = self.visit(ctx.classBody())
+        body = self.indent(body)
+        return textwrap.dedent(
+            f"""\
+            class {name}:
+        """) + body
+
+    def getDecorator(self, m: str) -> str:
+        if m == "static":
+            return "@staticmethod"
+        return ""
+
+    def visitMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
+        fname = ctx.IDENTIFIER().getText()
+        body = self.visit(ctx.methodBody())
+        params = self.visit(ctx.formalParameters())
+        body = self.indent(body)
+        modifiers = "\n".join([self.getDecorator(m) for m in ctx.parentCtx.parentCtx.modifiers])
+        return modifiers + "\n" + textwrap.dedent(
+            f"""\
+            def {fname}({params}):
+        """) + body
+
+    def visitClassBodyDeclaration(self, ctx: JavaParser.ClassBodyDeclarationContext):
+        ctx.modifiers = []
+        return super().visitClassBodyDeclaration(ctx)
+
+    def visitClassOrInterfaceModifier(self, ctx: JavaParser.ClassOrInterfaceModifierContext):
+        ctx.parentCtx.parentCtx.modifiers.append(ctx.getText())
+        return super().visitClassOrInterfaceModifier(ctx)
+
+    def visitMethodCall(self, ctx: JavaParser.MethodCallContext):
+        fname = ctx.IDENTIFIER().getText()
+        fname = self._dispatch(fname)
+        args = ctx.expressionList()
+        args = [args.expression(i) for i in range(args.getChildCount())]
+        args_str = ",".join([self.visit(a) for a in args])
+        return f"{fname}({args_str})"
+
+    def visitLiteral(self, ctx: JavaParser.LiteralContext):
+        if ctx.STRING_LITERAL:
+            return ctx.getText()
+        # Possibly other transformations go here
+        return ctx.getText()
+
+    def visitFormalParameterList(self, ctx: JavaParser.FormalParameterListContext):
+        params = [ctx.formalParameter(i) for i in range(ctx.getChildCount())]
+        return ",".join([self.visit(a) for a in params])
+
+    def visitFormalParameter(self, ctx: JavaParser.FormalParameterContext):
+        name = self.visit(ctx.variableDeclaratorId())
+        ptype = self.visit(ctx.typeType())
+        return f"{name}: {ptype}"
+
+    def visitVariableDeclaratorId(self, ctx: JavaParser.VariableDeclaratorIdContext):
+        return ctx.getText()
+
+    def visitClassOrInterfaceType(self, ctx: JavaParser.ClassOrInterfaceTypeContext):
+        java_type = ctx.getText()
+        return self.TYPE_MAP[java_type]
+
+    def visitTypeType(self, ctx: JavaParser.TypeTypeContext):
+        base = self.visit(ctx.classOrInterfaceType())
+        if ctx.LBRACK:
+            return f"List[{base}]"
+        return base
